ebd739202b8fb4ca278be0701b4c8bd10614ddc5
Deborah Cherotich <104517265+Debbie30@users.noreply.github.com>
2024-08-07 17:06:57 +0300

    Update ProductsPage.js

--- Client/src/Pages/ProductsPage.js
+++ Client/src/Pages/ProductsPage.js
@@ -1,17 +1,1 @@
-// src/pages/ProductsPage.js
-import React from 'react';
-// import ProductList from '../Components/Product/ProductList';
-// import Pagination from '../Components/Pagination';
-// import Filter from '../Components/Filter';
 
-const ProductsPage = () => {
-  return (
-    <div>
-      {/* <Filter />
-      <ProductList />
-      <Pagination /> */}
-    </div>
-  );
-};
-
-export default ProductsPage;
