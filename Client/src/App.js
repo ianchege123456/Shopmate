9f72b5acce051275730fd6c3860ad1a0da581c91
joan <joan.cherono@student.moringaschool.com>
2024-08-09 23:41:40 +0300

    Configure routes in app.j

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Profile from './Components/Profile';
+import Support from './Components/Support';
+import Error404 from './Components/PrivateRoute';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/support' element={<Support />} />
+      {/* Catch all undefined routes and render Error404 component */}
+      <Route path='*' element={<Error404 />} />
+    </Routes>
+  );
+}
+
+export default App;
